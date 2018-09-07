0992a874f2d07e91ecf831e45c00ba5d03cb1e4a
DeveshLoktra <devesh@loktra.com>
2018-09-07 15:38:06 +0530

    Initial Commit

---
+++ app/src/main/java/com/loktra/constants/AppConstants.java
@@ -0,0 +1,5 @@
+package com.loktra.constants;
+
+public class AppConstants {
+    public static final String API_URL = "http://api.tvmaze.com/shows";
+}
---
+++ app/src/main/java/com/loktra/tvmaze/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package com.loktra.tvmaze.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.loktra.tvmaze.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
